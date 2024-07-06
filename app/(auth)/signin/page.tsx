508ddf920366109d356a1ccde4fb5131a861f136
LilNait-S <style.damn.falling@gmail.com>
2024-07-06 11:53:27 -0500

    add

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,19 @@
+import { LayerForm } from "@/components/auth/layer-form";
+import { SignIn } from "@/components/auth/signin";
+
+export default function Page() {
+  return (
+    <LayerForm>
+      <header className="flex flex-col">
+        <h1 className="text-2xl sm:text-4xl font-bold leading-relaxed">
+          Iniciar sesión
+        </h1>
+        <p className="text-gray-500 leading-tight text-sm">
+          Bienvenido de nuevo. Por favor, ingresa tus credenciales para acceder
+          a tu cuenta.
+        </p>
+      </header>
+      <SignIn />
+    </LayerForm>
+  );
+}
