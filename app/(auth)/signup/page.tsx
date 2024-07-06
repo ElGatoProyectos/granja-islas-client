f5952f854b1802b4a3274acf02c5b7f8b014c2de
LilNait-S <style.damn.falling@gmail.com>
2024-07-06 11:53:29 -0500

    add

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,17 @@
+import { LayerForm } from "@/components/auth/layer-form";
+import { SignUp } from "@/components/auth/signup";
+
+export default function Page() {
+  return (
+    <LayerForm>
+      <header className="flex flex-col">
+        <h1 className="text-2xl sm:text-4xl font-bold mb-2">Registrate</h1>
+        <p className="text-gray-500 leading-tight text-sm">
+          Completa el formulario para crear tu cuenta y empezar a disfrutar de
+          nuestros servicios.
+        </p>
+      </header>
+      <SignUp />
+    </LayerForm>
+  );
+}
