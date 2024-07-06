1018b60703e05715f9a09c2fa008be9733b2c67b
LilNait-S <style.damn.falling@gmail.com>
2024-07-06 11:53:31 -0500

    add

---
+++ components/auth/layer-form.tsx
@@ -0,0 +1,21 @@
+export function LayerForm({ children }: { children: React.ReactNode }) {
+  return (
+    <section className="h-dvh flex justify-center items-center">
+      <section className="flex flex-col max-w-[500px] sm:p-12 bg-white gap-y-4 rounded-xl relative">
+        <div className="absolute -top-20 sm:-top-12 sm:left-12">
+          <div className="bg-primary flex justify-center items-center px-8 sm:px-10 py-3 pr-28 sm:pr-32 rounded-t-lg">
+            <span className="text-white text-xs sm:text-base text-balance">
+              Creado por <span className="font-semibold">Agencia Gato</span>
+            </span>
+          </div>
+          <img
+            src="/assets/gatologo.png"
+            alt="gato-logo"
+            className="absolute -top-9 right-2 sm:-right-2 sm:-top-11 h-[135px]"
+          />
+        </div>
+        {children}
+      </section>
+    </section>
+  );
+}
