62c5ef06cfef0d94c35955a2698a9dc4b34a773e
Lilnait-s <style.damn.falling@gmail.com>
2024-08-01 19:15:41 -0500

    add

--- context/supplier-context.tsx
+++ context/supplier-context.tsx
@@ -1,12 +1,9 @@
 "use client";
 
 import { backend_url } from "@/constants/config";
-import { UserTypeIn } from "@/types";
-
+import { SupplierTypeIn } from "@/components/suppliers/data-table/supplier-schema-table";
 import {
   createContext,
-  Dispatch,
-  SetStateAction,
   useCallback,
   useContext,
   useEffect,
@@ -14,13 +11,12 @@ import {
   useState,
 } from "react";
 import { useCompanySession } from "./company-context";
-import { SupplierTypeIn } from "@/components/suppliers/data-table/supplier-schema-table";
 import { useUserInfo } from "./user-context";
 
 interface SupplierContextType {
   suppliers: SupplierTypeIn[];
   loadingSuppliers: boolean;
-  getSuppliers: any;
+  getSuppliers: (page?: number, limit?: number) => Promise<void>;
 }
 
 export const supplierContext = createContext<SupplierContextType | null>(null);
@@ -35,33 +31,41 @@ export const SupplierProvider = ({
   const { tokenBack } = useUserInfo();
   const { company } = useCompanySession();
 
-  const getSuppliers = useCallback(async () => {
-    if (!company) return;
-    if (!tokenBack) return;
-    setLoading(true);
+  const getSuppliers = useCallback(
+    async (page?: number, limit?: number) => {
+      if (!company) return;
+      if (!tokenBack) return;
+      setLoading(true);
 
-    try {
-      const res = await fetch(`${backend_url}/api/suppliers`, {
-        method: "GET",
-        headers: {
-          Authorization: `Bearer ${tokenBack}`,
-          ruc: company.ruc,
-        },
-      });
+      try {
+        const queryParams = new URLSearchParams();
+        if (page) queryParams.append("page", page.toString());
+        if (limit) queryParams.append("limit", limit.toString());
 
-      if (!res.ok) {
-        throw new Error("Failed to fetch suppliers");
+        const url = `${backend_url}/api/suppliers?${queryParams.toString()}`;
+
+        const res = await fetch(url, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${tokenBack}`,
+            ruc: company.ruc,
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error("Failed to fetch suppliers");
+        }
+
+        const data = await res.json();
+        setSuppliers(data.payload.data.reverse());
+      } catch (error) {
+        console.error("Error to fetch data suppliers", error);
+      } finally {
+        setLoading(false);
       }
-
-      const data = await res.json();
-
-      setSuppliers(data.payload.data.reverse());
-    } catch (error) {
-      console.error("Error to fetch data suppliers", error);
-    } finally {
-      setLoading(false);
-    }
-  }, [company, tokenBack]);
+    },
+    [company, tokenBack]
+  );
 
   useEffect(() => {
     getSuppliers();
