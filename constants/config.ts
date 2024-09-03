2acb94559fe40545e0974152d322ce21f76cbc55
Lilnait-s <style.damn.falling@gmail.com>
2024-09-03 15:54:13 -0500

    add

--- constants/config.ts
+++ constants/config.ts
@@ -1,3 +1,3 @@
-export const backend_url = "http://localhost:4000";
+export const backend_url = "http://161.132.38.235:4000";
 export const backend_url_prod = "http://161.132.38.235:4000";
 export const backend_url_dev = "http://localhost:4000";
